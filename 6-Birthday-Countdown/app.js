3e0b59c645dc85ca0ee43f9297060eec63d58e21
Tolgahan Tom <106629813+tolgahantom@users.noreply.github.com>
2023-03-20 17:43:59 +0300

    adding and removing friend codes were written

--- 6-Birthday-Countdown/app.js
+++ 6-Birthday-Countdown/app.js
@@ -4,6 +4,42 @@ const addPersonPage = document.querySelector(".add-person-container");
 const addPersonButton = document.querySelector(".addPersonButton");
 const openPageButton = document.querySelector(".add-person-button");
 const closeAddButton = document.querySelector(".close-button");
+const friendList = JSON.parse(localStorage.getItem("friends") || "[]");
+
+showInFriendList = () => {
+  document.querySelectorAll(".person-list-item").forEach((item) => {
+    item.remove();
+  });
+  friendList.forEach((friend, index) => {
+    let liEl = `
+      <li class="person-list-item">
+        <h3 class="nickname">${friend.nickname}</h3>
+        <div class="settings-button">
+          <div onclick="removeFriend(${index})" class="startCountdownButton">
+            <i class="fa-solid fa-x"></i>
+          </div>
+          <div onclick="startCountdown(${index})" class="startCountdownButton">
+            <i class="fa-solid fa-paper-plane"></i>
+          </div>
+        </div>
+      </li>
+    `;
+    document.querySelector(".person-list").innerHTML += liEl;
+  });
+};
+
+removeFriend = (personIndex) => {
+  friendList.splice(personIndex, 1);
+  localStorage.setItem("friends", JSON.stringify(friendList));
+  showInFriendList();
+};
+
+startCountdown = (personIndex) => {
+  let selectedPerson = friendList[personIndex];
+  console.log(selectedPerson);
+};
+
+showInFriendList();
 
 toggleleftMenu = () => {
   leftMenu.classList.toggle("active");
@@ -17,6 +53,14 @@ addPersonToLS = () => {
     alert("You must write nickname and date");
     return;
   }
+  let personInfo = {
+    nickname: nickName,
+    birthday: date.toString(),
+  };
+  friendList.push(personInfo);
+  localStorage.setItem("friends", JSON.stringify(friendList));
+  showInFriendList();
+  closeAddButton.click();
 };
 
 openPage = () => {
